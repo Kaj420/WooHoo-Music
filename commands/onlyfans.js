55bb267cafe0d3f9b424b86872993eb01d001b3d
Kaj420 <74366526+Kaj420@users.noreply.github.com>
2021-01-02 00:43:04 +0100

    Add files via upload

---
+++ commands/onlyfans.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: "onlyfans",
+    description: "Shows the onlyfans",
+    async execute(message, args) {
+        return message.reply("Fijn dat je geinteresseerd bent in mijn onlyfans. Wel subben voor die top content he ;)\n \n https://onlyfans.com/kajdaddy")
+    }
+  };
+  
